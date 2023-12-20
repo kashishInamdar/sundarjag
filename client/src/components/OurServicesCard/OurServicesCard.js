ac5868db517debc749aae60f87b236dc998538a7
kashishInamdar <kashishinamdar2004@gmail.com>
2023-12-20 19:07:21 +0530

    why IDEVIFY

--- client/src/components/OurServicesCard/OurServicesCard.js
+++ client/src/components/OurServicesCard/OurServicesCard.js
@@ -1,13 +1,13 @@
 import "./OurServicesCard.css"
 
 
-const OurServicesCard = ({title , description , id , image })=>{
+const OurServicesCard = ({title , description , id , image})=>{
 
     return (
-        <div data-aos="flip-left " key={id} className="servicesCard">
-            <img  src={image} className="seviceLogo" />
-            <h3  >{title}</h3>
-            <p  >{description}</p>
+        <div key={id} className="servicesCard">
+             <img  src={image} className="seviceLogo" />
+            <h3 data-aos="zoom-in" data-aos-duration="1000" className="service-name" >{title}</h3>
+            <p data-aos="zoom-in" data-aos-duration="1000" className="service-description"  >{description}</p>
         </div>
     )
 }

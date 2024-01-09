8a7a3d0349ce033985ae07e6d71069def0de9dcd
kashishInamdar <kashishinamdar2004@gmail.com>
2024-01-09 22:57:27 +0530

    build for deploy

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ import express from "express" ;
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-import User from "./models/user.js";
-import md5 from "md5"
+import path from "path";
+
 
 import { PostApiLogin , PostApiSignup } from "./controller/Users.js";
 
-
-
 const app = express() ;
 app.use(express.json());
+
+const __dirname = path.resolve();
 
 const connectDB = async ()=>{
     const conn = await mongoose.connect(process.env.MONGOODB_URI) ;
@@ -20,9 +20,15 @@ const connectDB = async ()=>{
 }
 
 app.post("/api/signup" , PostApiSignup )
-
 app.post("/api/login" , PostApiLogin)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+    });
+  }
 
 const PORT = process.env.PORT || 5000 ;
 

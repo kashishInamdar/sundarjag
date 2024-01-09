826eb8b1ecdae2d1d8cf3e619ef8fa5f53db4d3d
kashishInamdar <kashishinamdar2004@gmail.com>
2024-01-09 18:10:50 +0530

    complete setup

---
+++ client/src/components/HomePoster/HomePoster.js
@@ -0,0 +1,16 @@
+import "./HomePoster.css"
+import flight from "./flight.jpg"
+import Timg1 from "./traveling-1.jpg"
+import Timg2 from "./traveling-2.jpg"
+import Timg5 from "./traveling-5.jpg"
+
+const HomePoster = () => {
+    return (
+        <>
+        
+
+        </>
+    )
+}
+
+export default HomePoster
